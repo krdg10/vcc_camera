0d52db8f1c8edbc08dff1afc75cd25649ec172ed
krdg10 <krdg10@live.com>
2019-06-21 07:48:27 -0400

    rota arrumada

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,8 @@ Auth::routes();
 
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('/motorista','MotoristaController');
+
+Route::get('/motorista','MotoristaController@index')->name('motorista');
+Route::post('/motorista','MotoristaController@store')->name('motorista.store');
+
 
