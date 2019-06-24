816a76000272679b5d81c629b92b8fc8c1d38c18
krdg10 <krdg10@live.com>
2019-06-24 10:55:07 -0400

    valida campos de view e motorista.

--- resources/views/carro/index.blade.php
+++ resources/views/carro/index.blade.php
@@ -1,14 +1,46 @@
 @extends('layouts.app')
 @section('content')
+<script>
+    function validaCampos() {
+        var ano = document.getElementsByName('ano')[0].value;
+        var placa = document.getElementsByName('placa')[0].value;
+        var validaAno = /^\d+$/;
+        var validaPlaca = /^[A-Z]{3}\-\d{4}$/;
+        
+       
+        if (validaAno.test(ano)==true && validaPlaca.test(placa)==true) {
+            return true;
+        } 
+        else if (validaPlaca.test(placa)==false) {
+            alert("Placa Inválida!");
+            return false;
+        }
+        else if (validaAno.test(ano)==false) {
+            alert("Verifique o ano. Apenas números são permitidos!");
+            return false;
+        }
+        else{
+            return false;
+        }
+    }
+    function caps(){
+        var placa = document.getElementsByName('placa')[0].value;
+        var placaCaps = placa.toUpperCase();
+        var insereTraco = placaCaps.replace(/^(\w{3})(\w{4})$/, "$1-$2");
+        document.getElementsByName('placa')[0].value=insereTraco;
+    }
+
+</script>
+<!-- não precede de \ [a-z]. Deixar \w na segunda pra por o traço independente do caracter --> 
 <div class="wrapper fadeInDown">
     <div id="formContent">
         <h3>Cadastrar Carro</h3> <hr>
-        <form method="POST" action="{{ route('carro.store') }}" enctype="multipart/form-data">
+        <form method="POST" action="{{ route('carro.store') }}" enctype="multipart/form-data" onsubmit="return validaCampos();">
         {{ csrf_field() }}
             <input type="text" placeholder="Nome" name="nome" class="form-control">
-            <input type="text" placeholder="Placa" name="placa" class="form-control">
+            <input type="text" placeholder="Placa" name="placa" class="form-control" maxlength="7" minlength="7" onblur="caps();">
             <input type="text" placeholder="Modelo" name="modelo" class="form-control">
-            <input type="text" placeholder="Ano" name="ano" class="form-control">
+            <input type="text" placeholder="Ano" name="ano" class="form-control" maxlength="4" minlength="4">
             <div id="formFooter">
                 <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Salvar </button>
             </div>
--- resources/views/motorista/index.blade.php
+++ resources/views/motorista/index.blade.php
@@ -1,15 +1,34 @@
 @extends('layouts.app')
 @section('content')
+<script>
+    function valida() {
+        var cpf = document.getElementsByName('cpf')[0].value;
+        var codigo_empresa = document.getElementsByName('codigo_empresa')[0].value;
+        var codigo_transdata = document.getElementsByName('codigo_transdata')[0].value;
+        
+        var valida = /^\d+$/;
+       
+        if (valida.test(cpf)==true && valida.test(codigo_empresa)==true && valida.test(codigo_transdata)==true ) {
+            return true;
+        } 
+        else {
+            alert("Verifique o CPF, Código VCC e Código Transdata. Apenas números são permitidos!");
+            return false;
+        }
+    }
+</script>
+<!-- script valida verifica se há apenas números positivos em cpf, codigo da empresa e codigo vcc. Alerta de tamanho não precisa script pois minlenght já faz isso -->
+
 <div class="wrapper fadeInDown">
     <div id="formContent">
         <h3>Cadastrar Motorista</h3> <hr>
-        <form method="POST" action="{{ route('motorista.store') }}" enctype="multipart/form-data">
+        <form method="POST" action="{{ route('motorista.store') }}" enctype="multipart/form-data" onsubmit="return valida();">
         {{ csrf_field() }}
             <input type="text" placeholder="Nome" name="nome" class="form-control">
-            <input type="text" placeholder="CPF" name="cpf" class="form-control">
-            <input type="text" placeholder="Data de Nascimento" name="data_nascimento" class="form-control">
-            <input type="text" placeholder="Código VCC" name="codigo_empresa" class="form-control">
-            <input type="text" placeholder="Código Transdata" name="codigo_transdata" class="form-control">
+            <input type="text" placeholder="CPF" name="cpf" class="form-control" maxlength="11" minlength="11">
+            <input type="date" placeholder="Data de Nascimento" name="data_nascimento" class="form-control">
+            <input type="text" placeholder="Código VCC" name="codigo_empresa" class="form-control" maxlength="4" minlength="4">
+            <input type="text" placeholder="Código Transdata" name="codigo_transdata" class="form-control" maxlength="5" minlength="5">
             <div id="formFooter">
                 <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Salvar </button>
             </div>
