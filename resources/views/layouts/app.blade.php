a563ecb921b9c0498bbdc9280795db53834d970d
krdg10 <krdg10@live.com>
2019-06-25 15:10:06 -0400

    CSS e mudança no endereço do logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="logo_vcc.png" id="icon" alt="User Icon" />
+                    <img src="/logo_vcc.png" id="icon" alt="User Icon" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
