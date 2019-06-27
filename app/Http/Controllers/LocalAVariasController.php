0f7680eb4adcd67f162a5393b6701272b0b8af87
lucascoml <lucasevangelista1998@gmail.com>
2019-06-27 10:14:45 -0300

    Configurado

--- app/Http/Controllers/LocalAVariasController.php
+++ app/Http/Controllers/LocalAVariasController.php
@@ -15,7 +15,17 @@ class LocalAVariasController extends Controller{
     }
 
     public function store(Request $request){
-        //
+        if(Local_avaria::where('local', 'like', '%'. $request->tipo .'%')->count() > 0)
+            return Metodos::retorno(0, 'Já existe "' . $request->tipo . '" cadastrado.');
+
+        try {
+            $local_avaria = new Local_avaria;
+            $local_avaria->tipo = $request->tipo;
+            $local_avaria->save();
+            return Metodos::retorno(1, 'Sucesso ao adicinar "' . $request->tipo . '".');
+        } catch (Exception $e) {
+            return Metodos::retorno(0, 'Erro ao inserir "' . $request->tipo . '".');
+        }
     }
 
     public function show($id=""){
