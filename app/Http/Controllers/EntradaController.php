0dbd3ac1035857230853df6e367a6cfdc65bd647
krdg10 <krdg10@live.com>
2019-07-16 16:56:54 -0400

    variaveis de novo, identação, uns comentários.

--- app/Http/Controllers/EntradaController.php
+++ app/Http/Controllers/EntradaController.php
@@ -26,29 +26,30 @@ class EntradaController extends Controller{
             $entradas = Entrada::orderBy('horario', 'desc')->paginate(5);
             return view('entrada.index', compact('entradas'));
         }
-       
+        $nome = $request->nome;
+        $horario = $request->horario;
+        $carro = $request->carro; //talvez n precisa pq o parametro do when pega o request do when. mas...
         $entradas = Entrada::join('motoristas', 'motoristas.id', '=', 'entradas.motorista_id')
-        ->join('carros', 'carros.id', '=', 'entradas.carro_id')
-        
-        ->when($request->verificado=='1', function($consulta){
-            $consulta->join('verificacoes', 'verificacoes.entrada_id', '=', 'entradas.id');
-        })
-        ->when($request->n_verificado=='1', function($consulta){
-            $consulta->whereNotIn('entradas.id', DB::table('verificacoes')->select('entrada_id'));
-        })
-        ->when($request->nome,function($consulta, $nome){
-            $consulta->where('motoristas.nome', 'like', '%' . $nome . '%');
-        })
-        ->when($request->horario,function($consulta, $horario){
-            $consulta->where('entradas.horario', $horario);
-        })
-        ->when($request->carro,function($consulta, $carro){
-            $consulta->where('carros.nome', $carro);
-        })
-        ->orderBy('entradas.horario', 'desc')
-        ->paginate(5, ['entradas.id', 'entradas.carro_id', 'entradas.motorista_id', 'entradas.horario']);
+                    ->join('carros', 'carros.id', '=', 'entradas.carro_id')
+                    ->when($request->verificado=='1', function($consulta){
+                        $consulta->join('verificacoes', 'verificacoes.entrada_id', '=', 'entradas.id');
+                    })
+                    ->when($request->n_verificado=='1', function($consulta){
+                        $consulta->whereNotIn('entradas.id', DB::table('verificacoes')->select('entrada_id'));
+                    })
+                    ->when($request->nome,function($consulta, $nome){
+                        $consulta->where('motoristas.nome', 'like', '%' . $nome . '%');
+                    })
+                    ->when($request->horario,function($consulta, $horario){
+                        $consulta->where('entradas.horario', $horario);
+                    })
+                    ->when($request->carro,function($consulta, $carro){
+                        $consulta->where('carros.nome', $carro);
+                    })
+                    ->orderBy('entradas.horario', 'desc')
+                    ->paginate(5, ['entradas.id', 'entradas.carro_id', 'entradas.motorista_id', 'entradas.horario']);
         //podia pegar carros.nome direto etc mas ia divergir do que tá na view.
-
+        //https://stackoverflow.com/questions/28176292/laravel-query-builder-returns-object-or-array
         return view('entrada.busca', ['entradas' => $entradas, 'nome' => $request->nome, 
         'horario' => $request->horario, 'carro' => $request->carro, 'verificado' => $request->verificado, 'n_verificado' => $request->n_verificado]);
     }
