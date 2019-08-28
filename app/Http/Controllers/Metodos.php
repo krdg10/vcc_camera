16fc4193598d3f1c5d7f51cba66800dc3f086536
krdg10 <krdg10@live.com>
2019-08-28 10:49:47 -0300

    Introdução do metódo retornoArray e retornoJson.

--- app/Http/Controllers/Metodos.php
+++ app/Http/Controllers/Metodos.php
@@ -6,6 +6,52 @@ use Illuminate\Support\Facades\DB;
 
 
 class Metodos extends Controller{
+
+    /**
+     *  Metodo que retorna uma array padrão.
+     *
+     * @version 1.0.0
+     * @require arquivo:metodo 0.0.0
+     * @param  int code
+     * @param  string message
+     * @param  array data
+     * @return array
+     *
+     *  @use arquivo:método:linha
+    */
+    public static function retornoArray(int $code, $message, $data = []){
+        $return = [
+            'code' => $code,
+            'message' => $message,
+            'data' => $data
+        ];
+        return $return;
+    }
+
+    
+    /**
+     * Retorna json.
+     * @version 0.0.0
+     * @require arquivo:método:0.0.0
+     *
+     * @param int $code
+     * @param string message
+     * @param array data
+     *
+     * @return Json
+     *
+     *  @use arquivo:método:linha
+     *  @use EntradaController:storeRbt:38
+    */
+    public static function retornoJson(int $code, $message, $data = []){
+        $retorno = [
+            'code' => $code,
+            'message' => $message,
+            'data' => $data
+        ];
+        return json_encode($retorno);
+    }
+
     public static function retorno(int $tipo, $msg, $dados = 0){
         $retorno = [
             'tipo' => $tipo,
