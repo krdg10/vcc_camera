56477eadb2f931cb3fc78d5e1e0698b7bd394e44
krdg10 <krdg10@live.com>
2019-08-29 14:59:49 -0400

    Coloquei imagem da camera no tamanho certo, dentro de um slideshow, com modal feito localmente em vez de metodo JS

--- resources/views/entrada/create.blade.php
+++ resources/views/entrada/create.blade.php
@@ -4,8 +4,6 @@
     function excluirElement(id){
         $('#'+id).remove();
     }
-
-    document.getElementoById('')
 </script>
 <div class="wrapper fadeInDown">
     <div id="formContent">
@@ -30,8 +28,49 @@
         
         <hr>
         <form method="POST" action="{{ route('entrada.store') }}" enctype="multipart/form-data">
-            <img src="http://192.168.254.193:94/snapshot.cgi?user=lan&pwd=lan&t=" style="height: 40px, width:50px">
-        {{ csrf_field() }}
+            @csrf
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" id="myImg" style="height: 300px" src="http://192.168.254.193:94/snapshot.cgi?user=lan&pwd=lan&t=" alt="Imagem da Câmera">
+                    </div>
+                </div>
+            </div>
+            <div id="modalImag" class="modal">
+
+                <!-- The Close Button -->
+                <span class="close">&times;</span>
+
+                <!-- Modal Content (The Image) -->
+                <img class="modal-content" id="img01">
+
+                <!-- Modal Caption (Image Text) -->
+                <div id="caption"></div>
+            </div>
+            <script>
+                // Get the modal
+                var modal = document.getElementById("modalImag");
+
+                // Get the image and insert it inside the modal - use its "alt" text as a caption
+                var img = document.getElementById("myImg");
+                var modalImg = document.getElementById("img01");
+                var captionText = document.getElementById("caption");
+                img.onclick = function(){
+                modal.style.display = "block";
+                modalImg.src = this.src;
+                captionText.innerHTML = this.alt;
+                }
+
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function() { 
+                modal.style.display = "none";
+                }
+            </script>
+
+
             <select class="MineSelect" name="motorista">
                 <option value="false"> Selecione um motorista</option>
                 @foreach($motorista as $Motorista) 
