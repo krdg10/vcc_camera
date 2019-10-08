4c5f4d2c004a45f3dbba608d703e2690a1e982e5
krdg10 <krdg10@live.com>
2019-10-08 17:16:20 -0400

    modularização da head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,59 +1,9 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>VCC Cam System</title>
-    <link rel="icon" href="{{ asset('logo_vcc.png') }}">
-
-    <!-- Scripts -->
-    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
-    <script type="text/javascript">
-        var csrfToken = "{{ csrf_token() }}"    ;
-    </script>
-
-    <script src="{{ asset('js/Metodos.js') }}" ></script>
-    <script src="{{ asset('js/Avaria.js') }}" ></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-    <script type="text/javascript" src="{{ asset('js/Xhttp.js') }}"></script>
-
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
-</head>
+@include('layouts.head')
 <body>
     <div id="app">
-        {{-- INCLUSÃO DO HEADER --}}
         @include('layouts.header')
-
-        {{-- 
-         * Mensagens.
-         *
-         * @version 0.0.0
-         * @require arquivo:método:0.0.0
-         * @param  \caminho  argument
-         *
-         *  @use arquivo:método:linha
-         *  @use MotoristaController:store:linha
-        --}}
-
-
-        {{-- ESPAÇO PARA OS CONTEÚDO --}}
         <main>
             @yield('content')
         </main>
