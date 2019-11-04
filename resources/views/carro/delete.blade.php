3c8be4e11b31457927e5103a6560772674973233
krdg10 <krdg10@live.com>
2019-11-04 10:14:46 -0400

    formgroup delete carro e motorista

--- resources/views/carro/delete.blade.php
+++ resources/views/carro/delete.blade.php
@@ -5,12 +5,26 @@
     <div id="formContent">
         <h3>Deletar Carro</h3> <hr>
         <form method="POST" action="{{ route('carro.destroy', $Carro->id) }}" enctype="multipart/form-data" >
-        
-            <input type="text" placeholder="Nome" name="nome" class="form-control" value="{{ $Carro->nome }}" disabled>
-            <input type="text" placeholder="Placa" name="placa" class="form-control" maxlength="7" minlength="7" value="{{ $Carro->placa }}" disabled>
-            <input type="text" placeholder="Modelo" name="modelo" class="form-control" value="{{ $Carro->modelo }}" disabled>
-            <input type="text" placeholder="Ano" name="ano" class="form-control" maxlength="4" minlength="4" value="{{ $Carro->ano }}" disabled>
-            <input type="text" placeholder="RFID" name="rfid" class="form-control" value="{{ $Carro->rfid }}" disabled>
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" placeholder="Nome" name="nome" class="form-control" value="{{ $Carro->nome }}" disabled>
+            </div>
+            <div class="form-group">
+                <label for="placa">Placa</label>
+                <input type="text" placeholder="Placa" name="placa" class="form-control" maxlength="7" minlength="7" value="{{ $Carro->placa }}" disabled>
+            </div>
+            <div class="form-group">
+                <label for="modelo">Modelo</label>
+                <input type="text" placeholder="Modelo" name="modelo" class="form-control" value="{{ $Carro->modelo }}" disabled>
+            </div>
+            <div class="form-group">
+                <label for="ano">Ano</label>
+                <input type="text" placeholder="Ano" name="ano" class="form-control" maxlength="4" minlength="4" value="{{ $Carro->ano }}" disabled>
+            </div>
+            <div class="form-group">
+                <label for="rfid">RFID</label>
+                <input type="text" placeholder="RFID" name="rfid" class="form-control" value="{{ $Carro->rfid }}" disabled>
+            </div>
             <div id="formFooter">
                 <button type="submit" id="submit" class="fadeIn fourth btn btn-primary" value="delete"> Excluir </button>
                 @method('delete')
--- resources/views/motorista/delete.blade.php
+++ resources/views/motorista/delete.blade.php
@@ -5,11 +5,26 @@
     <div id="formContent">
         <h3>Deletar Motorista</h3> <hr>
         <form method="POST" action="{{ route('motorista.destroy', ['id' => $Motorista->id]) }}" enctype="multipart/form-data" >
-            <input type="text" placeholder="Nome" name="nome" class="form-control" value="{{ $Motorista->nome }}" disabled>
-            <input type="text" placeholder="CPF" name="cpf" class="form-control" maxlength="11" minlength="11" value="{{ $Motorista->cpf }}" disabled>
-            <input type="text" placeholder="Data de Nascimento" name="data_nascimento" class="form-control" value="{{ $Motorista->data_nascimento }}" disabled>
-            <input type="text" placeholder="Código VCC" name="codigo_empresa" class="form-control" maxlength="4" minlength="4" value="{{ $Motorista->codigo_empresa }}" disabled>
-            <input type="text" placeholder="Código Transdata" name="codigo_transdata" class="form-control" maxlength="5" minlength="5" value="{{ $Motorista->codigo_transdata }}" disabled>
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" placeholder="Nome" name="nome" class="form-control" value="{{ $Motorista->nome }}" disabled>
+            </div>
+            <div class="form-group">
+                <label for="cpf">CPF</label>
+                <input type="text" placeholder="CPF" name="cpf" class="form-control" maxlength="11" minlength="11" value="{{ $Motorista->cpf }}" disabled>
+            </div>
+            <div class="form-group">
+                <label for="data_nascimento">Data de Nascimento</label>
+                <input type="date" placeholder="Data de Nascimento" name="data_nascimento" class="form-control" value="{{ $Motorista->data_nascimento }}" disabled>
+            </div>
+            <div class="form-group">
+                <label for="codigo_empresa">Código VCC</label>
+                <input type="text" placeholder="Código VCC" name="codigo_empresa" class="form-control" maxlength="4" minlength="4" value="{{ $Motorista->codigo_empresa }}" disabled>
+            </div>
+            <div class="form-group">
+                <label for="codigo_transdata">Código Transdata</label>
+                <input type="text" placeholder="Código Transdata" name="codigo_transdata" class="form-control" maxlength="5" minlength="5" value="{{ $Motorista->codigo_transdata }}" disabled>
+            </div>
             <div id="formFooter">
                 <button type="submit" id="submit" class="fadeIn fourth btn btn-primary" value="delete"> Deletar </button>
                 @method('delete')
