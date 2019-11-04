5de01d63c2fe4a1a4812381e17a821a9aa42f628
krdg10 <krdg10@live.com>
2019-11-04 09:41:43 -0400

    formgroup no cadastrar entrada

--- resources/views/entrada/create.blade.php
+++ resources/views/entrada/create.blade.php
@@ -73,23 +73,36 @@
                 <!-- Modal Caption (Image Text) -->
                 <div id="caption"></div>
             </div>
-
-            <select class="MineSelect" name="motorista" required>
-                <option value="false"> Selecione um motorista</option>
-                @foreach($motorista as $Motorista) 
-                    <option value="{{ $Motorista->id }}"> {{ $Motorista->nome }} </option>
-                @endforeach
-            </select>
-            <select class="MineSelect" name="carro" required> <!--tava form-control -->
-                <option value="false"> Selecione um carro</option>
-                @foreach($carro as $Carro)
-                    <option value="{{ $Carro->id }}"> {{ $Carro->nome }} - {{ $Carro->placa }} </option>
-                @endforeach
-            </select>
-
-            <input type="date" placeholder="Data" name="data" class="form-control" required>
-            <input type="time" placeholder="Horário" name="horario" class="form-control" required>
-            <input type="file" aria-label="foto" id="foto" name="fotos[]" class="form-control" accept="image/x-png, image/gif, image/jpeg, image/jpg" multiple required />
+            <div class="form-group">
+        		<label for="motorista">Motorista</label>
+                <select class="MineSelect" name="motorista" required>
+                    <option value="false"> Selecione um motorista</option>
+                    @foreach($motorista as $Motorista) 
+                        <option value="{{ $Motorista->id }}"> {{ $Motorista->nome }} </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+        		<label for="carro">Carro</label>
+                <select class="MineSelect" name="carro" required> <!--tava form-control -->
+                    <option value="false"> Selecione um carro</option>
+                    @foreach($carro as $Carro)
+                        <option value="{{ $Carro->id }}"> {{ $Carro->nome }} - {{ $Carro->placa }} </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+        		<label for="data">Data da Entrada</label>
+                <input type="date" placeholder="Data" name="data" class="form-control" required>
+            </div>
+            <div class="form-group">
+        		<label for="horario">Hora da Entrada</label>
+                <input type="time" placeholder="Horário" name="horario" class="form-control" required>
+            </div>
+            <div class="form-group">
+        		<label for="foto">Fotos</label>
+                <input type="file" aria-label="foto" id="foto" name="fotos[]" class="form-control" accept="image/x-png, image/gif, image/jpeg, image/jpg" multiple required />
+            </div>
             
             <div id="formFooter">
               <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Salvar </button>
