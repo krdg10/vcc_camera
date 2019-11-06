4a5b5830ff3eae1a3929db1038aef8d76f5492ae
krdg10 <krdg10@live.com>
2019-11-06 11:30:36 -0400

    tinha tirado 3.3 e colocado 3.4. ai coloquei 3.3 de novo e tirei o link do 3.4

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -16,13 +16,12 @@
 
     <script src="{{ asset('js/Metodos.js') }}" ></script>
     <script src="{{ asset('js/Avaria.js') }}" ></script>
-    <script src="{{ asset('js/jquery/jquery-3.4.1.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="{{ asset('js/bootstrap/bootstrap.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/Xhttp.js') }}"></script>
 
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="{{ asset('js/jquery-3.4.1.js') }}"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
